2994deeaf00edcfbcf87c1959291678cdc7a3541
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-22 11:36:14 +0200

    Add onject array

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
 import React, { useState } from "react";
 import ColorCards from "./Components/ColorCards";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [colorCodes, setColorCodes] = useState([
+    {
+      id: nanoid(),
+      colorCode: "#fe9f8e",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#212b36",
+    },
+    {
+      id: nanoid(),
+      colorCode: "#e36da3",
+    },
+  ]);
+
+  console.log(colorCodes);
+
   return (
     <>
+    
       <ColorCards />
     </>
   );
